8cf3de708794bed131dd0ff757ee44e39b55d9d3
Dan Kadera <me@subsonic.cz>
2026-02-22 21:54:19 +0100

    fixups

--- src/lib/components/views/index.ts
+++ src/lib/components/views/index.ts
@@ -3,4 +3,3 @@ export { default as Done } from './done.svelte';
 export { default as Intro } from './intro.svelte';
 export { default as Ready } from './ready.svelte';
 export { default as Recording } from './recording.svelte';
-1;
